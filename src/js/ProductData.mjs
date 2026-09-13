19aa034ea349c46940c0764f2e00d86d7dac2b7b
jesamonen <jesamonen01@gmail.com>
2026-09-13 21:43:31 +0100

    resolving file path

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,21 +1,46 @@
+// function convertToJson(res) {
+//   if (res.ok) {
+//     return res.json();
+//   } else {
+//     throw new Error("Bad Response");
+//   }
+// }
+
+// export default class ProductData {
+//   constructor(category) {
+//     this.category = category;
+//     this.path = `/json/${this.category}.json`;
+//   }
+//   getData() {
+//     return fetch(this.path)
+//       .then(convertToJson)
+//       .then((data) => data);
+//   }
+//   async findProductById(id) {
+//     const products = await this.getData();
+//     return products.find((item) => item.Id === id);
+//   }
+// }
+
 function convertToJson(res) {
   if (res.ok) {
     return res.json();
   } else {
-    throw new Error("Bad Response");
+    throw new Error(`Bad Response: ${res.status} ${res.statusText}`);
   }
 }
 
 export default class ProductData {
   constructor(category) {
     this.category = category;
-    this.path = `/json/${this.category}.json`;
+    // Uses Vite's module resolution to find src/json relative to this file
+    this.path = new URL(`../json/${this.category}.json`, import.meta.url).href;
   }
-  getData() {
-    return fetch(this.path)
-      .then(convertToJson)
-      .then((data) => data);
+
+  async getData() {
+    return fetch(this.path).then(convertToJson);
   }
+
   async findProductById(id) {
     const products = await this.getData();
     return products.find((item) => item.Id === id);
