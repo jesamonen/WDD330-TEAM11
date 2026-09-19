bfb0ed01b4e4e0e44a3732120f35617048c96ea7
jesamonen <jesamonen01@gmail.com>
2026-09-19 16:15:58 +0100

    Add dynamic product details page

---
+++ src/js/ProductDetails.mjs
@@ -0,0 +1,50 @@
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+
+export default class ProductDetails {
+  constructor(productId, dataSource) {
+    this.productId = productId;
+    this.dataSource = dataSource;
+    this.product = {};
+  }
+
+  async init() {
+    this.product = await this.dataSource.findProductById(this.productId);
+    this.renderProductDetails();
+  }
+
+  addProductToCart() {
+    let cart = getLocalStorage("so-cart");
+
+    if (!Array.isArray(cart)) {
+      cart = [];
+    }
+
+    cart.push(this.product);
+    setLocalStorage("so-cart", cart);
+  }
+
+  renderProductDetails() {
+    document.querySelector(".product-detail h3").textContent =
+      this.product.Brand;
+
+    document.querySelector(".product-detail h2").textContent =
+      this.product.Name;
+
+    const image = document.querySelector(".product-detail img");
+    image.src = this.product.Image;
+    image.alt = this.product.Name;
+
+    document.querySelector(".product-card__price").textContent =
+      `$${this.product.FinalPrice}`;
+
+    document.querySelector(".product__color").textContent =
+      this.product.Colors[0].ColorName;
+
+    document.querySelector(".product__description").textContent =
+      this.product.Description;
+
+    document
+      .getElementById("addToCart")
+      .addEventListener("click", () => this.addProductToCart());
+  }
+}
