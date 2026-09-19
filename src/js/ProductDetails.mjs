628c1c83103c37f95d84ff26510a6da6b0659ab9
jesamonen <jesamonen01@gmail.com>
2026-09-19 21:09:14 +0100

    Complete W02 requirements

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -25,7 +25,7 @@ export default class ProductDetails {
 
   renderProductDetails() {
     document.querySelector(".product-detail h3").textContent =
-      this.product.Brand;
+     this.product.Brand.Name;
 
     document.querySelector(".product-detail h2").textContent =
       this.product.Name;
@@ -36,6 +36,15 @@ export default class ProductDetails {
 
     document.querySelector(".product-card__price").textContent =
       `$${this.product.FinalPrice}`;
+        const discount = document.querySelector(".product-detail__discount");
+
+        if (this.product.FinalPrice < this.product.SuggestedRetailPrice) {
+        discount.textContent = `Save $${(
+            this.product.SuggestedRetailPrice - this.product.FinalPrice
+        ).toFixed(2)}`;
+        } else {
+        discount.textContent = "";
+        }
 
     document.querySelector(".product__color").textContent =
       this.product.Colors[0].ColorName;
