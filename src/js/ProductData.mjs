63da1e9980c0b1fa0980c2de731cce747354dd8d
jesamonen <jesamonen01@gmail.com>
2026-09-20 10:32:03 +0100

    Fix product API base URL

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,17 +1,20 @@
-const baseURL = import.meta.env.VITE_SERVER_URL;
-
 function convertToJson(res) {
   if (res.ok) {
     return res.json();
-  } else {
-    throw new Error("Bad Response");
   }
+
+  throw new Error("Bad Response");
 }
 
+
+const baseURL = import.meta.env.VITE_SERVER_URL;
+
+
 export default class ProductData {
+
   async getData(category) {
     const response = await fetch(
-      `${baseURL}products/search/${category}`,
+      `${baseURL}products/search/${category}`
     );
 
     const data = await convertToJson(response);
@@ -19,8 +22,11 @@ export default class ProductData {
     return data.Result;
   }
 
+
   async findProductById(id) {
-    const response = await fetch(`${baseURL}product/${id}`);
+    const response = await fetch(
+      `${baseURL}product/${id}`
+    );
 
     const data = await convertToJson(response);
 
